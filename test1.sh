4f248501648a0fa5f8c4f60e679da58059bb668f
Ubuntu <ubuntu@ip-172-31-32-61.eu-west-1.compute.internal>
2016-02-04 08:37:29 +0000

    test node

--- test1.sh
+++ test1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -i /home/circleci/node-js-test circleci@54.194.98.52 "npm install && forever start server.js"
+ssh -i /home/circleci circleci@54.194.98.52 "cd node-js-test && npm install && forever start server.js"
